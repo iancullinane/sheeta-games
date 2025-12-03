0bde5c9702d866e58556128b7f2c5bffe51f871d
Ian Cullinane <ian.cullinane@gmail.com>
2025-12-03 14:29:53 -0500

    small change

--- bin/environment.ts
+++ bin/environment.ts
@@ -9,21 +9,21 @@ import { GodotAssetsStack } from "../lib/storage/godot-assets-stack";
 import { SheetaComputeStack } from "../lib/compute/sheeta-compute-stack";
 
 var mainConfig = {
-  name: "adventurebrave",
-  network: {
-    domains: [
-      {
-        name: "adventurebrave.com",
-      },
-    ],
-  },
+	name: "adventurebrave",
+	network: {
+		domains: [
+			{
+				name: "adventurebrave.com",
+			},
+		],
+	},
 };
 
 const app = new cdk.App();
 
 const defaultTags = {
-  Environment: "dev",
-  Project: "adventurebrave",
+	Environment: "dev",
+	Project: "adventurebrave",
 };
 
 // These tags will be inherited by all child stacks and constructs
@@ -35,68 +35,61 @@ cdk.Tags.of(app).add("Project", defaultTags.Project);
 
 // Setup initial environment config and so on
 const foundation = new Foundation(app, "FoundationStack", {
-  network: mainConfig.network,
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+	network: mainConfig.network,
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
 });
 
 // Create shared API Gateway that other stacks can use
 const apiGateway = new ApiGatewayFoundation(app, "ApiGatewayFoundation", {
-  projectName: mainConfig.name,
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+	projectName: mainConfig.name,
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
 });
 
 // Lambda endpoint using shared API Gateway
 new LambdaEndpoint(app, "SheetaGamesStack", {
-  projectName: mainConfig.name,
-  restApi: apiGateway.restApi,
-  routePath: "/hello",
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+	projectName: mainConfig.name,
+	restApi: apiGateway.restApi,
+	routePath: "/hello",
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
 });
-
-
-
-// 
-
-
-
 
 
 // Option 2: Use the new domainName parameter to look up the hosted zone
 // This approach doesn't require the Foundation stack reference
 /*
 new LambdaEndpoint(app, "SheetaGamesEndpoint", {
-  projectName: mainConfig.name,
-  domainName: "adventurebrave.com", // The domain name to look up
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+	projectName: mainConfig.name,
+	domainName: "adventurebrave.com", // The domain name to look up
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
 });
 */
 
 // Add Godot assets storage stack
 new GodotAssetsStack(app, "GodotAssetsStack", {
-  projectName: mainConfig.name,
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+	projectName: mainConfig.name,
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
 });
 
 // Add compute stack with ALB and EC2
 new SheetaComputeStack(app, "SheetaComputeStack", {
-  projectName: mainConfig.name,
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+	projectName: mainConfig.name,
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
 });
